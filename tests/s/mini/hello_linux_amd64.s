6fc6d2f76c7d509bb1ce15ad4a9edf165b963d6d
pad <yoann.padioleau@gmail.com>
2025-06-16 16:39:46 +0200

    use 6l -X to simplify also tests/s/mini/

--- tests/s/mini/hello_linux_amd64.s
+++ tests/s/mini/hello_linux_amd64.s
@@ -6,6 +6,7 @@
 // -------------------------------------------
 
 // we use _start and not main to avoid triggering Go-specific stuff in 6l
+// update: can also now use 6l -X to disable the go-specific stuff
 TEXT    _start+0(SB), 7, $0
 
         // Allocate space for return address (CALL pushes return addr)
@@ -38,40 +39,6 @@ TEXT    write+0(SB), 7, $0
         SYSCALL
         RET
 
-
-// -------------------------------------------
-// to shup up the linker
-// -------------------------------------------
-
-TEXT runtime·morestack00(SB), 7, $0
-    RET
-
-TEXT runtime·morestack10(SB), 7, $0
-    RET
-
-TEXT runtime·morestack01(SB), 7, $0
-    RET
-
-TEXT runtime·morestack11(SB), 7, $0
-    RET
-
-TEXT runtime·morestack8(SB), 7, $0
-    RET
-
-TEXT runtime·morestack16(SB), 7, $0
-    RET
-
-TEXT runtime·morestack24(SB), 7, $0
-    RET
-
-TEXT runtime·morestack32(SB), 7, $0
-    RET
-
-TEXT runtime·morestack40(SB), 7, $0
-    RET
-
-TEXT runtime·morestack48(SB), 7, $0
-    RET
 
 // -------------------------------------------
 // msg: must split into 8-byte chunks
