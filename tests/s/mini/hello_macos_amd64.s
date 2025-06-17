183a23c23b84b9f9f32b763aaa22b4fbc6d171b5
pad <yoann.padioleau@gmail.com>
2025-06-17 10:15:37 +0200

    try hello_macos_amd64.s

---
+++ tests/s/mini/hello_macos_amd64.s
@@ -0,0 +1,43 @@
+
+TEXT    _start(SB), 7, $0
+        SUBQ    $16, SP
+        LEAQ    msg(SB), AX
+        MOVQ    AX, 0(SP)
+        MOVQ    $14, 8(SP)
+        CALL    write(SB)
+        ADDQ    $16, SP
+        CALL    exit(SB)
+
+TEXT	notok(SB),7,$0
+	MOVL	$0xf1, BP
+	MOVQ	BP, (BP)
+	RET
+
+// -------------------------------------------
+// write(buf *byte, len int)
+// -------------------------------------------
+TEXT    write(SB), 7, $0
+	MOVL	$1, DI		// arg 1 fd
+	MOVQ	8(SP), SI		// arg 2 buf
+	MOVL	16(SP), DX		// arg 3 count
+	MOVL	$(0x2000000+4), AX	// syscall entry
+	SYSCALL
+	JCC	2(PC)
+	CALL	notok(SB)
+	RET
+
+
+// -------------------------------------------
+// exit()
+// -------------------------------------------
+TEXT    exit(SB), 7, $0
+	MOVL	$0, DI		// arg 1 exit status
+	MOVL	$(0x2000000+1), AX	// syscall entry
+	SYSCALL
+
+// -------------------------------------------
+// msg: must split into 8-byte chunks
+// -------------------------------------------
+DATA    msg+0(SB)/8, $"Hello, w"
+DATA    msg+8(SB)/6, $"orld\n"
+GLOBL   msg(SB), $14
