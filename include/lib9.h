94cea33ab82b7b3d75d82876b270eb366ae58afb
pad <yoann.padioleau@gmail.com>
2025-06-27 18:40:48 +0200

    include/lib9.h

---
+++ include/lib9.h
@@ -0,0 +1,5 @@
+// This was not part of the go original source but pad added it
+// to imitate what the other kencc (and especially inferno-os/utils) has
+// been doing.
+#include <u.h>
+#include <libc.h>
