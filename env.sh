979c4a8060716435e5f6fb45a53a6274cf11d386
pad <yoann.padioleau@gmail.com>
2025-09-17 17:32:24 +0200

    disable src/libmach/linux.c for now for arm64
    Imitate what they did for freebsd.c with empty
    functions

--- env.sh
+++ env.sh
@@ -1,8 +1,8 @@
 #TODO: source mkconfig? or merge with mkconfig?
 
-# bin/ for (promoted) mk and rc and ROOT/amd64/... for 6a/6c/6l/...
+# bin/ for (promoted) mk and rc and ROOT/{amd,arm}64/... for 6a/6c/6l/...
 # called during mk test
-export PATH=`pwd`/bin:`pwd`/ROOT/amd64/bin:$PATH
+export PATH=`pwd`/bin:`pwd`/ROOT/amd64/bin:`pwd`/ROOT/arm64/bin:$PATH
 # for mk to find rc
 export MKSHELL=`pwd`/bin/rc
 # for rc to find its init file
