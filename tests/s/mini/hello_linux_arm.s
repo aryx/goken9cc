93a418eb89637003e99988a42079ae05b1f036b2
pad <yoann.padioleau@gmail.com>
2025-09-23 12:27:43 +0200

    try hello_linux_arm.s but fails

---
+++ tests/s/mini/hello_linux_arm.s
@@ -0,0 +1,37 @@
+// -------------------------------------------
+// main procedure
+// -------------------------------------------
+
+TEXT _start+0(SB), 7, $20
+	//TODO: port xdefine("setR12",...) from layout.c in principia to goken
+	//MOVW	$setR12(SB), R12
+        /* prepare the system call PWRITE(1,&hello,12, 00) */
+        MOVW $1, R1
+        MOVW R1, 4(R13)
+        MOVW $hello(SB), R1
+        MOVW R1, 8(R13)
+        MOVW $12, R1
+        MOVW R1, 12(R13)
+        MOVW $0, R1
+        MOVW R1, 16(R13)
+        MOVW R1, 20(R13)
+        MOVW $9 /*PWRITE*/, R0
+        /* system call */
+        SWI $0
+        BL exit(SB)
+        RET /* not reached */
+loop:
+        B loop
+
+TEXT exit(SB), 7, $4
+        /* prepare the system call EXITS(0) */
+        MOVW $0, R1
+        MOVW R1, 4(R13)
+        MOVW $3 /*EXITS*/, R0
+        /* system call */
+        SWI $0
+        RET /* not reached */
+
+GLOBL   hello(SB), $12
+DATA    hello+0(SB)/8, $"hello wo"
+DATA    hello+8(SB)/4, $"rld\n"
