21f47148a2f04ed264e7337e1db58a7e47fdf4cd
pad <yoann.padioleau@gmail.com>
2025-09-26 12:05:22 +0200

    5c_: progress on tests/c/mini2/helloprintf
    test plan:
    cd test/c/mini2
    mk helloprintf_arm_.exe
    helloprintf_arm_.exe
    => Hello World: ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
    
    so incorrect :( at least no segfault, but
    not much better than 5a/5c/5l

--- tests/c/mini2/helloprintf.c
+++ tests/c/mini2/helloprintf.c
@@ -1,6 +1,8 @@
 //TODO: merge once iar is fixed to handle .7 objects
 #ifndef arm64
+#ifndef arm_
 #pragma lib "libprint.a"
+#endif
 #endif
 
 #include "minilibc.h"
@@ -12,6 +14,6 @@ void main() {
 
 // just to see the generated assembly code when using 6c/7c -S
 void test() {
-  write(0, "test\n", 5);
+  write(1, "test\n", 5);
   exit(42);
 }
--- tests/c/mini2/linux_arm.s
+++ tests/c/mini2/linux_arm.s
@@ -1,18 +1,35 @@
 
+TEXT _start(SB), 1, $0
+#ifdef arm_
+	MOVW $setR12(SB), R12
+#endif
+	BL main(SB)
+
+
 TEXT    exit+0(SB), 7, $0
+#ifdef arm_
+	//with 5c_, the first arg is already passed via R0
+#else
         MOVW    status+0(FP), R0
+#endif
         MOVW    $1, R7          // syscall number 1 = sys_exit
         SWI     $0
 	RET // never reached
 
 TEXT    panic+0(SB), 7, $0
+#ifdef arm_
+#else
         MOVW    status+0(FP), R0
+#endif
         MOVW    $1, R7          // syscall number 1 = sys_exit
         SWI     $0
 	RET // never reached
 
 TEXT write+0(SB), 7, $0
+#ifdef arm_
+#else
         MOVW    fd+0(FP), R0
+#endif
         MOVW    buf+4(FP), R1
         MOVW    count+8(FP), R2
         MOVW    $4, R7          // syscall number 4 = sys_write
@@ -36,55 +53,55 @@ TEXT 	_mod+0(SB), 7, $0
 	RET
 TEXT 	_modu+0(SB), 7, $0
 	RET
-
-//Float and 64bits stuff
-TEXT 	_si2v+0(SB), 7, $0
-	RET
-TEXT 	_ui2v+0(SB), 7, $0
-	RET
-TEXT 	_v2si+0(SB), 7, $0
-	RET
-TEXT 	_v2sl+0(SB), 7, $0
-	RET
-TEXT 	_v2uc+0(SB), 7, $0
-	RET
-TEXT 	_v2ul+0(SB), 7, $0
-	RET
-TEXT 	_v2ui+0(SB), 7, $0
-	RET
-TEXT 	_p2v+0(SB), 7, $0
-	RET
-
-TEXT 	_modvu+0(SB), 7, $0
-	RET
-TEXT 	_divvu+0(SB), 7, $0
-	RET
-
-TEXT 	_addv+0(SB), 7, $0
-	RET
-TEXT 	_subv+0(SB), 7, $0
-	RET
-TEXT 	_andv+0(SB), 7, $0
-	RET
-TEXT 	_xorv+0(SB), 7, $0
-	RET
-TEXT 	_orv+0(SB), 7, $0
-	RET
-TEXT 	_eqv+0(SB), 7, $0
-	RET
-TEXT 	_lshv+0(SB), 7, $0
-	RET
-TEXT 	_rshlv+0(SB), 7, $0
-	RET
-
-
-TEXT 	_lov+0(SB), 7, $0
-	RET
-TEXT 	_ltv+0(SB), 7, $0
-	RET
-TEXT 	_vasop+0(SB), 7, $0
-	RET
-TEXT 	_hiv+0(SB), 7, $0
-	RET
-TEXT 	_sfloat+0(SB), 7, $0
-	RET
+//
+////Float and 64bits stuff
+//TEXT 	_si2v+0(SB), 7, $0
+//	RET
+//TEXT 	_ui2v+0(SB), 7, $0
+//	RET
+//TEXT 	_v2si+0(SB), 7, $0
+//	RET
+//TEXT 	_v2sl+0(SB), 7, $0
+//	RET
+//TEXT 	_v2uc+0(SB), 7, $0
+//	RET
+//TEXT 	_v2ul+0(SB), 7, $0
+//	RET
+//TEXT 	_v2ui+0(SB), 7, $0
+//	RET
+//TEXT 	_p2v+0(SB), 7, $0
+//	RET
+//
+//TEXT 	_modvu+0(SB), 7, $0
+//	RET
+//TEXT 	_divvu+0(SB), 7, $0
+//	RET
+//
+//TEXT 	_addv+0(SB), 7, $0
+//	RET
+//TEXT 	_subv+0(SB), 7, $0
+//	RET
+//TEXT 	_andv+0(SB), 7, $0
+//	RET
+//TEXT 	_xorv+0(SB), 7, $0
+//	RET
+//TEXT 	_orv+0(SB), 7, $0
+//	RET
+//TEXT 	_eqv+0(SB), 7, $0
+//	RET
+//TEXT 	_lshv+0(SB), 7, $0
+//	RET
+//TEXT 	_rshlv+0(SB), 7, $0
+//	RET
+//
+//
+//TEXT 	_lov+0(SB), 7, $0
+//	RET
+//TEXT 	_ltv+0(SB), 7, $0
+//	RET
+//TEXT 	_vasop+0(SB), 7, $0
+//	RET
+//TEXT 	_hiv+0(SB), 7, $0
+//	RET
+//TEXT 	_sfloat+0(SB), 7, $0
+//	RET
