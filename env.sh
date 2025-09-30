fd4b069a8182184ff2d1392ccaa25caeae4c7c66
pad <yoann.padioleau@gmail.com>
2025-09-30 09:04:38 +0200

    misc

--- env.sh
+++ env.sh
@@ -3,6 +3,7 @@
 # bin/ for (promoted) mk and rc and ROOT/{amd,arm}64/... for 6a/6c/6l/...
 # called during mk test
 export PATH=`pwd`/bin:`pwd`/ROOT/amd64/bin:`pwd`/ROOT/arm64/bin:$PATH
+
 # for mk to find rc
 export MKSHELL=`pwd`/bin/rc
 # for rc to find its init file
