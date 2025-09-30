c83a0eecc0bf0f6903eb58c5bd6f9be923fdd169
pad <yoann.padioleau@gmail.com>
2025-09-30 09:04:47 +0200

    started scripts/promote-for-principia.sh
    utilities that are needed to compile principia-softwarica
    to replace the programs in my fork-kencc

---
+++ scripts/promote-for-principia.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+if [ ! -f configure ]; then
+	echo 'this script must be run from the project root'
+	exit 1
+fi
+if [ ! -f mkconfig ]; then
+	echo 'you must run ./configure first'
+	exit 1
+fi
+. ./mkconfig
+set -x
+
+cp ROOT/$GOARCH/bin/5c_ bin/5c
+cp ROOT/$GOARCH/bin/5a_ bin/5a
+cp ROOT/$GOARCH/bin/5l_ bin/5l
+rm -f ROOT/$GOARCH/bin/5[acl]_
+rm -f ROOT/$GOARCH/bin/5[acl]
+
+cp ROOT/$GOARCH/bin/iar bin/iar
+cp ROOT/$GOARCH/bin/yacc bin/iyacc
