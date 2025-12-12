94ca9d872a5ab20a9182ad2188ead56aa6f7e589
pad <yoann.padioleau@gmail.com>
2025-12-12 18:45:58 +0100

    more comments on vlong_max.c

--- tests/c/misc/vlong_max.c
+++ tests/c/misc/vlong_max.c
@@ -5,7 +5,16 @@ typedef long long vlong;
 
 vlong test() {
     // 8c -S should generate some
+    //TEXT	test+0(SB),0,$0
+	//MOVL	.ret+0(FP),AX
+	//MOVL	$-1,(AX)
+	//MOVL	$2147483647,4(AX)
 
+    // but instead was generating
+    //TEXT	test+0(SB),0,$0
+	//MOVL	.ret+0(FP),AX
+	//MOVL	$9223372036854775807,(AX)
+	//MOVL	$2147483647,4(AX)
     
     return VLONG_MAX;
 }
