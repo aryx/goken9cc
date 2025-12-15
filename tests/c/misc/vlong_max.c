37eca18735caac2866ead9b63b8311cfbfe39e49
pad <yoann.padioleau@gmail.com>
2025-12-15 11:17:24 +0100

    comment

--- tests/c/misc/vlong_max.c
+++ tests/c/misc/vlong_max.c
@@ -4,17 +4,20 @@
 typedef long long vlong;
 
 vlong test() {
-    // 8c -S should generate some
+    // 8c -S should display some
     //TEXT	test+0(SB),0,$0
 	//MOVL	.ret+0(FP),AX
 	//MOVL	$-1,(AX)
 	//MOVL	$2147483647,4(AX)
 
-    // but instead was generating
+    // but instead was displaying
     //TEXT	test+0(SB),0,$0
 	//MOVL	.ret+0(FP),AX
 	//MOVL	$9223372036854775807,(AX)
 	//MOVL	$2147483647,4(AX)
+
+    // because of some %ld and long instead of %d and int32
+    // for Adr offset in gc.h
     
     return VLONG_MAX;
 }
