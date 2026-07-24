6cc2b44895fa028cc1921f87d5ae3cb4cebb226d
pad <yoann.padioleau@gmail.com>
2026-07-24 18:38:43 +0200

    misc

--- lib_toolchain/libmach/ureg6.h
+++ lib_toolchain/libmach/ureg6.h
@@ -8,20 +8,25 @@ struct Ureg
 	uvlong	r10;
 	uvlong	r9;
 	uvlong	r8;
+
 	uvlong	di;
 	uvlong	si;		/* ... */
 	uvlong	bp;		/* ... */
 	uvlong	nsp;
+
 	uvlong	bx;		/* ... */
 	uvlong	dx;		/* ... */
 	uvlong	cx;		/* ... */
 	uvlong	ax;		/* ... */
+
 	uvlong	gs;		/* data segments */
 	uvlong	fs;		/* ... */
 	uvlong	es;		/* ... */
 	uvlong	ds;		/* ... */
+
 	uvlong	trap;		/* trap type */
 	uvlong	ecode;		/* error code (or zero) */
+
 	uvlong	pc;		/* pc */
 	uvlong	cs;		/* old context */
 	uvlong	flags;		/* old flags */
