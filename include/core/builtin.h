ffefc2062f5972f4a21cf25335353b817c7d34ec
pad <yoann.padioleau@gmail.com>
2026-07-25 13:28:28 +0200

    misc

---
+++ include/core/builtin.h
@@ -0,0 +1,23 @@
+
+// ----------------
+// Builtin types
+// ----------------
+
+// bool (not builtin, in base/bool.h)
+// char
+// int, short, long, long long
+// float, double
+
+// ??
+
+// ----------------
+// Builtin operators
+// ----------------
+
+// +, -, *, /, % for many numeric types
+// ==, !=
+// <, >, <=, >= again for many types
+
+// ...
+
+
