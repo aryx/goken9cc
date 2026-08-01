7a835dcdff3474735a11f93cceef45a0b981ed50
pad <yoann.padioleau@gmail.com>
2026-08-01 12:21:08 +0200

    move bool def from core/types.h to base/bool.h

--- include/base/bool.h
+++ include/base/bool.h
@@ -1,14 +1,19 @@
 
 // a bool type! not part of C89, appeared in C11?
-// Using int8 for a bool would be more compact but has consequences
-// when using it in arithmetic context.
-// Safer to use a full int for now.
 
-typedef int bool;
+// bool is needed to compile {mk,rc,5c,8c,...} which
+// comes from pad's principia which use a few extra C types
+// (I like types, and I especially don't like abusing ints for everything)
+
+// Note that using 'uint8' for bool and not 'int' has consequences! DO NOT
+// transform what you think is a bool like 'int flag;' to a bool when
+// this variable is actually used in arithmetic context. It led to
+// many regressions.
+// alt: typedef int bool; // would cause less regressions, but not as clean
+typedef u8 bool;
 enum _bool {
-  false = 0,
-  true = 1
+	false = 0,
+	true = 1,
 };
 
-
-typedef uchar bool_byte;
+//typedef uchar bool_byte;
--- include/core/types.h
+++ include/core/types.h
@@ -1,5 +1,6 @@
 
-// those are portable defs across architectures
+// Those are portable defs across architectures
+// The per-arch specific are in include/arch/<arch>/u.h
 
 typedef signed char schar;
 typedef unsigned char uchar;
@@ -11,7 +12,7 @@ typedef long long vlong;
 typedef unsigned long long uvlong;
 
 
-// the [su]8 defined in u.h
+// the [su]8 are defined in <u.h>
 //old: used to be s8int, u8int, etc. but shorted s8/u8/... like in Rust and Zig.
 typedef s8 int8;
 typedef u8 uint8;
@@ -22,7 +23,7 @@ typedef u32 uint32;
 typedef s64 int64;
 typedef u64 uint64;
 
-// intptr defined in u.h
+// intptr defined in <u.h>
 //pad: I added that, for ed.c
 //alt: remove this typedef and use ptrdiff_t which is standard
 // but types without the _t suffix is more plan9ish
@@ -33,16 +34,4 @@ typedef intptr ptrdiff;
 // so just alias it here instead of editing that ported file.
 typedef uintptr uintptr_t;
 
-#ifndef __bool_true_false_are_defined
-//Note that it is dangerous to use u8 for a bool and to switch some
-// 'int flag;' to 'bool flag;' in some legacy code because this code
-// might actually use this flag in an integer context and using a bool
-// might truncate some operations now.
-// So take care when transitioning code to bool!
-typedef u8 bool;
-enum {
-	false = 0,
-	true = 1,
-};
-#define __bool_true_false_are_defined 1
-#endif
+// bool now in base/bool.h
